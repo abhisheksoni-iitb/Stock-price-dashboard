991d218468f25ad9ede8f99f90b98ae5969f42ba
Abhishek Soni <50213022+abhisheksoni-iitb@users.noreply.github.com>
2020-12-03 01:21:24 +0530

    BSE stock inputs added

--- app.py
+++ app.py
@@ -1,131 +1,106 @@
-# Importing required libraries
 import streamlit as st
-import cv2
-import numpy as np
-import os
-import PIL
-from PIL import Image
+import pandas as pd
 import base64
-from io import BytesIO
-
-# Load Yolo
-net = cv2.dnn.readNetFromDarknet("model/yolov3.cfg",
-                                     "model/yolov3.weights")
-
-classes = []
-with open("model/classes.txt", "r") as f:
-    classes = [line.strip() for line in f.readlines()]
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-colors = np.random.uniform(0, 255, size=(len(classes), 3))
-
-def about():
-	st.write('')
-
-def get_optimal_font_scale(text, width):
-    for scale in reversed(range(0, 10, 1)):
-        textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=scale/10, thickness=1)
-        new_width = textSize[0][0]
-    # print(new_width)
-        if (new_width <= width):
-            return scale/10
-    return 1
-
-def main():
-    st.title("Object detection App")
-    st.write("")
-
-    activities = ["Home", "About"]
-    choice = st.sidebar.selectbox("Section", activities)
-
-    if choice == "Home":
-
-        confidence_threshold = st.sidebar.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.01)                
-        # You can specify more file types below if you want
-        image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
-
-        if image_file is not None:
-
-
-            image = Image.open(image_file)
-            img = np.array(image.convert('RGB'))
-
-            img = cv2.resize(img, None, fx=0.9, fy=0.9)
-            height, width, channels = img.shape
-            blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-            net.setInput(blob)
-            outs = net.forward(output_layers)
-
-            # if st.button("Process"):
-            
-            class_ids = []
-            confidences = []
-            boxes = []
-            for out in outs:
-                for detection in out:
-                    scores = detection[5:]
-                    class_id = np.argmax(scores)
-                    confidence = scores[class_id]
-                    if confidence > confidence_threshold:
-                        # Object detected
-                        center_x = int(detection[0] * width)
-                        center_y = int(detection[1] * height)
-                        w = int(detection[2] * width)
-                        h = int(detection[3] * height)
-
-                        # Rectangle coordinates
-                        x = int(center_x - w / 2)
-                        y = int(center_y - h / 2)
-
-                        boxes.append([x, y, w, h])
-                        confidences.append(float(confidence))
-                        class_ids.append(class_id)
-
-            indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-            # print(indexes)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            
-            for i in range(len(boxes)):
-                if i in indexes:
-                    x, y, w, h = boxes[i]
-                    label = str(classes[class_ids[i]])
-                    label+=', '
-                    label+= str((round(confidences[i],2)*100))
-                    label+= '%'
-                    color = colors[class_ids[i]]
-                    cv2.rectangle(img, (x, y), (x + w, y + h), color, 3)
-                    # cv2.putText(img, label, (x, y + 15), font,  0.5, color, 2)
-            
-                    # Draw black background rectangle
-                    cv2.rectangle(img, (x, y), (x + int(w),y + int(h/15)), (255,255,255), -1)
-
-                    # Add text
-                    fontscale = get_optimal_font_scale(label, width)
-                    cv2.putText(img, label, (x + int(w/20),y + int(h/20)), font,  fontscale, (0,0,0),lineType=cv2.LINE_AA)
-                    # cv2.putText(image, "THICC flower", (x + int(w/10),y + int(h/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
-           
-            st.image(img, use_column_width = True)
-
-            def download_link(img):
-                """Generates a link allowing the PIL image to be downloaded
-                in:  PIL image
-                out: href string
-                """
-                buffered = BytesIO()
-                img.save(buffered, format="JPEG")
-                img_str = base64.b64encode(buffered.getvalue()).decode()
-                href = f'<a href="data:file/jpg;base64,{img_str}" download="processed.png">Download File</a>'
-                return href
-
-            if st.button('Create Download Link'):
-                result = Image.fromarray(img)
-                st.markdown(download_link(result), unsafe_allow_html=True)
-
-    elif choice == "About":
-        about()
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+# import yfinance as yf
+import quandl
+import plotly.graph_objects as go
+from plotly import tools
+import plotly.offline as py
+import plotly.express as px
 
 
 
+st.title('Stock Price Dashboard')
 
-if __name__ == "__main__":
-    main()
+st.markdown("""
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+""")
+
+stock = st.selectbox('Select the Stock', [None,'RELIANCE', 'HDFCBANK', 'ADANIPORTS', 'ITC', 'SBI', 'IOC', 'RBLBANK'])
+
+stock_map = {
+    'RELIANCE':"BSE/BOM500325", 
+    'HDFCBANK':"BSE/BOM500180", 
+    'ADANIPORTS':"BSE/BOM532921", 
+    'ITC':"BSE/BOM500875", 
+    'SBI':"BSE/BOM500112", 
+    'IOC':"BSE/BOM540065", 
+    'RBLBANK':"BSE/BOM540065"
+}
+# Web scraping of S&P 500 data
+#
+if stock is not None:
+    # @st.cache
+    def get_data(stock_code):
+        df = quandl.get(stock_code, authtoken="_j-x2o8Gk93gkkqyE6tG")
+
+        #cleaning the data
+        features_to_drop = ['No. of Shares', 'No. of Trades','Spread H-L', 'Spread C-O']
+        df.drop(features_to_drop,1,inplace=True)
+        df.rename(columns = {'WAP': 'Volume' },
+                inplace = True)
+        # df['Volume'] = df['WAP']
+        # df.drop(['WAP'],1, inplace = True)
+        df.dropna(inplace=True)
+        return df
+
+    df = get_data(stock_map[stock])
+
+    def plots(df):
+        cndlstck = go.Candlestick(
+            x = df.index, 
+            open = df['Open'], 
+            high = df['High'], 
+            low = df['Low'], 
+            close = df['Close'], showlegend = True
+            )
+
+        df['EMA_9'] = df['Close'].ewm(5).mean().shift()
+        df['SMA_50'] = df['Close'].rolling(50).mean().shift()
+        df['SMA_100'] = df['Close'].rolling(100).mean().shift()
+
+
+
+        fig = go.Figure(data = [cndlstck])
+        # fig.add_volume(go.Scatter(x=list(df.index), y=list(df['High']),name='High'))
+        fig.add_trace(go.Scatter(x=df.index, y=df.EMA_9, name='EMA 9', opacity=0.2))
+        fig.add_trace(go.Scatter(x=df.index, y=df.SMA_50, name='SMA 50', opacity=0.2))
+        fig.add_trace(go.Scatter(x=df.index, y=df.SMA_100, name='SMA 100', opacity=0.2))
+        fig.add_trace(go.Scatter(x=df.index, y=df.Close, name='Close', line_color='dimgray', opacity=0.2))
+
+        fig.update_layout(
+            title="Time series with range slider and selectors",
+            yaxis_title = 'Stock Price',
+            xaxis_title = 'Date'
+
+        )
+
+        fig.layout.xaxis.type = 'category'
+        fig.add_trace(go.Bar(x=df.index, y=df['Volume'], name='Volume', marker_color='black', opacity=0.05))
+        # Add range slider
+        fig.update_layout(
+            xaxis=dict(
+                rangeselector=dict(
+                    buttons=list([
+                        dict(count=1,label="1m",step="month",stepmode="backward"),
+                        dict(count=6,label="6m",step="month",stepmode="backward"),
+                        dict(count=1,label="YTD",step="year",stepmode="todate"),
+                        dict(count=1,label="1y",step="year",stepmode="backward"),
+                        dict(step="all")
+                    ])
+                ),
+                rangeslider=dict(visible=True),
+                type="date"
+            )
+        )
+        return fig
+        # fig.update_layout(rangeslider=dict(visible=True))
+        #   fig.show()
+
+    fig = plots(df)
+    st.plotly_chart(fig)
