79baddb1f956f3764d8a0c497277c475c229b785
Abhishek Soni <50213022+abhisheksoni-iitb@users.noreply.github.com>
2020-12-03 11:47:16 +0530

    Change NSE library to NSEpy

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ import plotly.graph_objects as go
 from plotly import tools
 import plotly.offline as py
 import plotly.express as px
-from nsetools import Nse
-
+from nsepy import get_history
+import datetime
+from datetime import date
 
 
 st.title('Stock Price Dashboard')
@@ -29,6 +30,56 @@ stock_map = {
     'IOC':"BSE/BOM540065", 
     'RBLBANK':"BSE/BOM540065"
 }
+
+def plots(df):
+    cndlstck = go.Candlestick(
+        x = df.index, 
+        open = df['Open'], 
+        high = df['High'], 
+        low = df['Low'], 
+        close = df['Close']
+        )
+
+    df['EMA_9'] = df['Close'].ewm(5).mean().shift()
+    df['SMA_50'] = df['Close'].rolling(50).mean().shift()
+    df['SMA_100'] = df['Close'].rolling(100).mean().shift()
+
+
+
+    fig = go.Figure(data = [cndlstck])
+    # fig.add_volume(go.Scatter(x=list(df.index), y=list(df['High']),name='High'))
+    fig.add_trace(go.Scatter(x=df.index, y=df.EMA_9, name='EMA 9', opacity=0.2))
+    fig.add_trace(go.Scatter(x=df.index, y=df.SMA_50, name='SMA 50', opacity=0.2))
+    fig.add_trace(go.Scatter(x=df.index, y=df.SMA_100, name='SMA 100', opacity=0.2))
+    fig.add_trace(go.Scatter(x=df.index, y=df.Close, name='Close', line_color='dimgray', opacity=0.2))
+
+    fig.update_layout(
+        title="Time series with range slider and selectors",
+        yaxis_title = 'Stock Price',
+        xaxis_title = 'Date'
+
+    )
+
+    fig.layout.xaxis.type = 'category'
+    # fig.add_trace(go.Bar(x=df.index, y=df['Volume'], name='Volume', marker_color='black', opacity=0.05))
+    # Add range slider
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,label="1m",step="month",stepmode="backward"),
+                    dict(count=6,label="6m",step="month",stepmode="backward"),
+                    dict(count=1,label="YTD",step="year",stepmode="todate"),
+                    dict(count=1,label="1y",step="year",stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(visible=True),
+            type="date"
+        )
+    )
+    fig.update_layout(width=800,height=600)
+    return fig
 
 
 if stock_exchange is not None:
@@ -62,55 +113,7 @@ if stock_exchange is not None:
         # df2 = df2.round(decimals=1)
         st.dataframe(df2[::-1],width=1000)
 
-        def plots(df):
-            cndlstck = go.Candlestick(
-                x = df.index, 
-                open = df['Open'], 
-                high = df['High'], 
-                low = df['Low'], 
-                close = df['Close'], showlegend = True
-                )
-
-            df['EMA_9'] = df['Close'].ewm(5).mean().shift()
-            df['SMA_50'] = df['Close'].rolling(50).mean().shift()
-            df['SMA_100'] = df['Close'].rolling(100).mean().shift()
-
-
-
-            fig = go.Figure(data = [cndlstck])
-            # fig.add_volume(go.Scatter(x=list(df.index), y=list(df['High']),name='High'))
-            fig.add_trace(go.Scatter(x=df.index, y=df.EMA_9, name='EMA 9', opacity=0.2))
-            fig.add_trace(go.Scatter(x=df.index, y=df.SMA_50, name='SMA 50', opacity=0.2))
-            fig.add_trace(go.Scatter(x=df.index, y=df.SMA_100, name='SMA 100', opacity=0.2))
-            fig.add_trace(go.Scatter(x=df.index, y=df.Close, name='Close', line_color='dimgray', opacity=0.2))
-
-            fig.update_layout(
-                title="Time series with range slider and selectors",
-                yaxis_title = 'Stock Price',
-                xaxis_title = 'Date'
-
-            )
-
-            fig.layout.xaxis.type = 'category'
-            fig.add_trace(go.Bar(x=df.index, y=df['Volume'], name='Volume', marker_color='black', opacity=0.05))
-            # Add range slider
-            fig.update_layout(
-                xaxis=dict(
-                    rangeselector=dict(
-                        buttons=list([
-                            dict(count=1,label="1m",step="month",stepmode="backward"),
-                            dict(count=6,label="6m",step="month",stepmode="backward"),
-                            dict(count=1,label="YTD",step="year",stepmode="todate"),
-                            dict(count=1,label="1y",step="year",stepmode="backward"),
-                            dict(step="all")
-                        ])
-                    ),
-                    rangeslider=dict(visible=True),
-                    type="date"
-                )
-            )
-            fig.update_layout(width=800,height=600)
-            return fig
+        
             # fig.update_layout(rangeslider=dict(visible=True))
             #   fig.show()
         # df2 = df[df.index >]
@@ -118,42 +121,23 @@ if stock_exchange is not None:
         st.plotly_chart(fig)
 
     elif (stock is not None) and (stock_exchange ==  'National Stock Exchange'):
-        nse = Nse()
-        
-        stock_map = {
-                    'RELIANCE':"RELIANCE", 
-                    'HDFCBANK':"HDFCBANK", 
-                    'ADANIPORTS':"ADANIPORTS", 
-                    'ITC':"ITC", 
-                    'SBI':"SBIN", 
-                    'IOC':"IOC", 
-                    'RBLBANK':"RBLBANK"
-                }
 
-        stock_dict = nse.get_quote(stock_map[stock])
+        def get_data():
+            df = get_history(symbol="SBIN", start=date(2016,1,1), end=datetime.datetime.now().date())
 
-        cols = ['open', 'dayHigh', 'dayLow', 'closePrice',
-         'totalTradedVolume',  'deliveryQuantity', 'deliveryToTradedQuantity']
-        columns = ['Open', 'High', 'Low', 'Close',
-         'Volume',  'Deliverable Quantity', '% Deli. Qty to Traded Qty']
-        data_dict = {}
+            #cleaning the data
+            features_to_drop = ['Symbol', 'Series', 'Prev Close', 'Last',
+                                'VWAP',  'Turnover', 'Trades', ]
+            df.drop(features_to_drop,1,inplace=True)
+            # df.rename(columns = {'WAP': 'Volume' },
+            #         inplace = True)
+            # df['Volume'] = df['WAP']
+            # df.drop(['WAP'],1, inplace = True)
+            # df.dropna(inplace=True)
+            return df
+        df = get_data()
+        st.dataframe(df[::-1],width=1000)
 
-        for i,col in enumerate(cols):
-            if stock_dict[col] ==0:
-                stock_dict[col] = '--'
-            # label = str(columns[i]) + ' ' + str(stock_dict[col])
-            # st.success(label)
-            data_dict.update({columns[i]:stock_dict[col]})
-        
-
-        df = pd.DataFrame.from_dict(data_dict,orient='index',
-        columns=[ 'Value'])
-        df.rename_axis("Parameter", axis="columns",inplace = True)
-        df.style.set_properties(subset=[0], **{'text-align': 'left'},inplace = True)
-        st.dataframe(df[::-1],width=2000)
-        # for i,col in enumerate(cols):
-        #     if stock_dict[col] ==0:
-        #         stock_dict[col] = '--'
-        #     label = str(columns[i]) + ' ' + str(stock_dict[col])
-        #     st.success(label)
+        fig = plots(df)
+        st.plotly_chart(fig)
         
